3c7ada6480edd1646d8426e02edb8f9dd4859f1e
GambuzX <mg_mesquita@hotmail.com>
2019-03-11 23:00:09 +0000

    Started project; Parsed command line arguments

---
+++ forensic.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "utility.h"
+
+int main(int argc, char * argv[], char * envp[]) {
+
+    if (argc < 2 || argc > 8) {
+        printf("Usage: ./forensic [-r] [-h [md5[,sha1[,sha256]]] [-o <outfile>] [-v] <file|dir>\n");
+        exit(1);
+    }
+
+    /* 0 | r | h | md5 | sha1 | sha256 | o | v */
+    char flags = 0;
+
+    /* File or directory specified */
+    char * start_point = argv[argc-1];
+
+    /* Output file (NULL if -o not set) */
+    char * out_file = NULL;
+
+    for (int i = 1 ; i < argc-1; i++) {
+        /* Found -r flag */
+        if (!strcmp(argv[i], "-r")) {
+            if (flags & FLAGS_R) {
+                printf("Repeated -r flag\n");
+                exit(1);
+            }
+            /* Update flag */
+            flags |= FLAGS_R;
+        }
+
+        /* Found -v flag */
+        else if (!strcmp(argv[i], "-v")) {
+            if (flags & FLAGS_V) {
+                printf("Repeated -v flag\n");
+                exit(1);
+            }
+            /* Update flag */
+            flags |= FLAGS_V;
+        }
+
+        /* Found -h flag */
+        else if (!strcmp(argv[i], "-h")) {
+            if (flags & FLAGS_H) {
+                printf("Repeated -h flag\n");
+                exit(1);
+            }
+            /* Update flag */
+            flags |= FLAGS_H;
+
+            /* Iterate through the algorithms specified */
+            char * algorithms = argv[i+1];
+            char * next = strtok(algorithms, ",");
+            if (next == NULL) {
+                printf("-h flag requires an algorithm\n");
+                exit(1);
+            }
+            while(next != NULL) {
+                if (!strcmp(next, "md5")) {
+                    if (flags & FLAGS_MD5) {
+                        printf("Repeated md5 algorithm\n");
+                        exit(1);
+                    }
+                    /* Update flag */
+                    flags |= FLAGS_MD5;
+                }
+                else if (!strcmp(next, "sha1")) {
+                    if (flags & FLAGS_SHA1) {
+                        printf("Repeated sha1 algorithm\n");
+                        exit(1);
+                    }
+                    /* Update flag */
+                    flags |= FLAGS_SHA1;
+                }
+                else if (!strcmp(next, "sha256")) {
+                    if (flags & FLAGS_SHA256) {
+                        printf("Repeated sha256 algorithm\n");
+                        exit(1);
+                    }
+                    /* Update flag */
+                    flags |= FLAGS_SHA256;
+                }
+                else {
+                    printf("Invalid algorithm specified: %s\n", next);
+                    exit(1);
+                }
+
+                /* Get next string */
+                next = strtok(NULL, ",");
+            }
+            /* Skip next element */
+            i++;
+        }
+        
+        /* Found -o flag */
+        else if (!strcmp(argv[i], "-o")) {
+            if (flags & FLAGS_O) {
+                printf("Repeated -o flag\n");
+                exit(1);
+            }
+            /* Check for outfile name */
+            if (i >= argc-2) {
+                printf("-o flag requires an outfile name\n");
+                exit(1);
+            }
+
+            /* Update flag and out_file */
+            flags |= FLAGS_O;
+            out_file = argv[i+1];
+            
+            /* Skip next element */
+            i++;
+        }
+
+        /* Do not accept any other option */
+        else {
+            printf("Invalid option: %s\n", argv[i]);
+            exit(1);
+        }
+    }
+
+    printf("%2x\n", flags);
+    if (out_file != NULL) printf("%s\n", out_file);
+    printf("%s\n", start_point);
+
+    return 0;
+}
---
+++ utility.h
@@ -0,0 +1,17 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+
+#define BIT(n) 0x1<<n
+
+/* Bits for the used flags */
+#define FLAGS_R BIT(6)
+#define FLAGS_H BIT(5)
+#define FLAGS_MD5 BIT(4)
+#define FLAGS_SHA1 BIT(3)
+#define FLAGS_SHA256 BIT(2)
+#define FLAGS_O BIT(1)
+#define FLAGS_V BIT(0)
+
+
+#endif

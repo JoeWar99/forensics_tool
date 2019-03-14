9e9ce7039e01215feefa1ef6cfbfa3908cf22ef1
GambuzX <mg_mesquita@hotmail.com>
2019-03-14 22:51:53 +0000

    Started concatenating output to a string

--- forensic.c
+++ forensic.c
@@ -11,7 +11,39 @@
 #include <time.h>
 #include <wait.h>
 
+// TODO free memory after using concat
+void concat(char* str1, char* str2, int n) {
+	const size_t len1 = strlen(str1);
+    const size_t len2 = n < strlen(str2) ? n : strlen(str2);
+    str1 = (char*) realloc(str1, len1 + len2 + 1); // +1 for the null-terminator
 
+	/* Check for errors */
+	if (str1 == NULL) {
+		perror("realloc");
+		exit(1);
+	}
+
+	/* Concatenate str2 */
+    memcpy(str1 + len1, str2, len2);
+	str1[len1+len2] = '\0';
+
+	// TODO
+	// Ele aqui fica bem, mas depois na funcao principal nao fica
+	printf("%s\n", str1);
+}
+
+/*
+void append_string(char* str1, char* str2) { 
+	printf(">1 %s\n", str1);
+	//char * prev = str1;
+	str1 = concat(str1, str2, strlen(str2)); 
+	//free(prev);
+	printf(">2 %s\n", str1);
+}
+void append_string_n(char* str1, char* str2, int n) { 
+	str1 = concat(str1, str2, n); 
+}
+*/
 int file_forensic(char flag, char *start_point, struct stat stat_buf, char *outfile)
 {
 	// file_name,file_type,file_size,file_access,file_created_date,file_modification_date,md5,sha1,sha256
@@ -25,6 +57,7 @@ int file_forensic(char flag, char *start_point, struct stat stat_buf, char *outf
 	int n;
 	FILE *fp;
 	int filedes;
+	char * ret_string = (char *) malloc(1);
 	
 	
 	if(flag & FLAGS_O){
@@ -40,28 +73,40 @@ int file_forensic(char flag, char *start_point, struct stat stat_buf, char *outf
         printf("Error opening file pipe!\n");
         return -1;
     }
+	write(STDOUT_FILENO, ret_string, strlen(ret_string));
 
 	int started = 0;
 	char * next;
     while (fgets(buf, BUFFER_SIZE, fp) != NULL) {
 		if (!started) {
 			next = strtok(buf, ":");
-			write(STDOUT_FILENO, next, strlen(next));
-			write(STDOUT_FILENO, ",", 1);
+			concat(ret_string, next, strlen(next));
+			concat(ret_string, ",", 1);
+			//write(STDOUT_FILENO, next, strlen(next));
+			//write(STDOUT_FILENO, ",", 1);
 			next = strtok(NULL, ",");
 			started = 1;
 		}
 		else
 			next = strtok(buf, ",");
 
+		printf(">1 %s\n", ret_string);
 		while(next != NULL){
 			if('\n' == next[strlen(next) - 1]) {
-				write(STDOUT_FILENO, next+1, strlen(next)-2);
+				printf(">4 %s\n", ret_string);
+				concat(ret_string, next+1, strlen(next+1)-1);
+				printf(">5 %s\n", ret_string);
+				//write(STDOUT_FILENO, next+1, strlen(next)-2);
 			}
 			else {
-				write(STDOUT_FILENO, next+1, strlen(next));
+				concat(ret_string, next+1, strlen(next+1));
+				printf(">2 %s\n", ret_string);
+				//write(STDOUT_FILENO, next+1, strlen(next));
+				// TODO above should be strlen(next) - 1 ????
 			}
-			write(STDOUT_FILENO, ",", 1);
+			concat(ret_string, ",", 1);
+			printf(">3 %s\n", ret_string);
+			//write(STDOUT_FILENO, ",", 1);
 			next = strtok(NULL, ",");
 		}
     }
@@ -72,7 +117,8 @@ int file_forensic(char flag, char *start_point, struct stat stat_buf, char *outf
     }
 
 	n = sprintf(buf, "%ld,", stat_buf.st_size);
-	write(STDOUT_FILENO, buf, n);
+	concat(ret_string, buf, n);
+	//write(STDOUT_FILENO, buf, n);
 
 	// PERMISSOES  , DEPOIS TEMOS QUE UTILIZAR PROVAVELMENTE O WRITE() PARA ISTO
 	// printf( (S_ISDIR(stat_buf.st_mode)) ? "d" : "-");
@@ -86,20 +132,21 @@ int file_forensic(char flag, char *start_point, struct stat stat_buf, char *outf
     // printf( (stat_buf.st_mode & S_IWOTH) ? "w" : "-");
     // printf( (stat_buf.st_mode & S_IXOTH) ? "x" : "-");
 
+/*
     printf( (stat_buf.st_mode & S_IRUSR) ? "r" : "");
     printf( (stat_buf.st_mode & S_IWUSR) ? "w" : "");
     printf( (stat_buf.st_mode & S_IXUSR) ? "x" : "");
-    printf(","); 
+    printf(","); */
 	fflush(stdout);
 
 	// TODO: Both dates are the same
 	ts = *localtime(&stat_buf.st_ctime);
 	strftime(buf, sizeof(buf), "%Y-%m-%dT%H:%M:%S,", &ts);
-	write(STDOUT_FILENO, buf, strlen(buf));
+	//write(STDOUT_FILENO, buf, strlen(buf));
 
 	ts = *localtime(&stat_buf.st_mtime);
 	strftime(buf, sizeof(buf), "%Y-%m-%dT%H:%M:%S", &ts);
-	write(STDOUT_FILENO, buf, strlen(buf));
+	//write(STDOUT_FILENO, buf, strlen(buf));
 
 	if (flag & FLAGS_SHA1)
 	{
@@ -167,11 +214,13 @@ int file_forensic(char flag, char *start_point, struct stat stat_buf, char *outf
 		}
 	}
 
-	write(STDOUT_FILENO, "\n", 1);
+	//write(STDOUT_FILENO, "\n", 1);
 
 	if(flag & FLAGS_O){
 		close(filedes);
 	}
 
+	write(STDOUT_FILENO, ret_string, strlen(ret_string));
+	free(ret_string);
 	return 0;
 }
